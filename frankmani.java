ff0d1b53ef3f706e5872c3ec78aa8997356b5333
FMN27 <124590504+FMN27@users.noreply.github.com>
2024-06-03 00:16:35 -0400

    Update frankmani.java

--- frankmani.java
+++ frankmani.java
@@ -5,6 +5,13 @@ import robocode.*;
 
 public class frankmani extends Robot {
 	public void run() {
+		setBodyColor(Color.grey);
+		setGunColor(Color.green);
+		setRadarColor(Color.green);
+		setBulletColor(Color.red);
+		setScanColor(Color.yellow);
+
+		//loop
 		while (true) {
 			//the robots do a zig zag
 			for (int i = 0; i < 5; i++){
@@ -17,7 +24,7 @@ public class frankmani extends Robot {
 
 			turnRight(360);
 
-            //the repition of the robot's movement mimics a type of zig-zag
+            //the repetition of the robot's movement mimics a type of zig-zag
 			ahead(110);
 			turnRight(55);
 		}
